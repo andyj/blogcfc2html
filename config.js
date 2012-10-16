7282a6d66d7bc8f7a8a65bb643405d7a5c3b5bef
Andy Jarrett <mail@andyjarrett.co.uk>
2012-10-16 14:50:03 +0100

    tidying up code

--- config.js
+++ config.js
@@ -3,17 +3,18 @@ var config = {}
 config.outPath = "blogentries/";
 
 // SQL to grab the blog entries
-config.sql = "SELECT \
-								REPLACE(REPLACE(REPLACE(title,'\,',''),'''',''),':','-') AS title ,\
-								CONCAT_WS('<br>', body,morebody) AS fullbody ,\
-								alias ,\
-								lower(CONCAT_WS('-', TRIM(LEADING '0' FROM date_format(posted, '%Y')),\
-								TRIM(LEADING '0' FROM date_format(posted, '%c')),\
-								TRIM(LEADING '0' FROM date_format(posted, '%d')),alias)) AS htmlFileName,\
-								views,\
-								posted\
-							FROM tblblogentries\
-							ORDER BY posted ASC";
+config.sql = "
+	SELECT \
+		REPLACE(REPLACE(REPLACE(title,'\,',''),'''',''),':','-') AS title ,\
+		CONCAT_WS('<br>', body,morebody) AS fullbody ,\
+		alias ,\
+		lower(CONCAT_WS('-', TRIM(LEADING '0' FROM date_format(posted, '%Y')),\
+		TRIM(LEADING '0' FROM date_format(posted, '%c')),\
+		TRIM(LEADING '0' FROM date_format(posted, '%d')),alias)) AS htmlFileName,\
+		views,\
+		posted\
+	FROM tblblogentries\
+	ORDER BY posted ASC";
 
 config.mysql = {
 	host     : 'localhost',
