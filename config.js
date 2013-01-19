c88e621cc65c4cfca9fda3be3d367952f5460384
Andy Jarrett <mail@andyjarrett.co.uk>
2013-01-19 09:22:12 +0000

    fixed formatting issues

--- config.js
+++ config.js
@@ -3,24 +3,26 @@ var config = {};
 config.outPath = "blogentries/";
 
 // SQL to grab the blog entries
-config.sql = "
-	SELECT \
-		REPLACE(REPLACE(REPLACE(title,'\,',''),'''',''),':','-') AS title ,\
-		CONCAT_WS('<br>', body,morebody) AS fullbody ,\
-		alias ,\
-		lower(CONCAT_WS('-', TRIM(LEADING '0' FROM date_format(posted, '%Y')),\
-		TRIM(LEADING '0' FROM date_format(posted, '%c')),\
-		TRIM(LEADING '0' FROM date_format(posted, '%d')),alias)) AS htmlFileName,\
-		views,\
-		posted\
-	FROM tblblogentries\
-	ORDER BY posted ASC";
+
+config.sql =
+		"SELECT " +
+		"REPLACE(REPLACE(REPLACE(title,'\,',''),'''',''),':','-') AS title , " +
+		"CONCAT_WS('<br>', body,morebody) AS fullbody , " +
+		"alias , " +
+		"lower(CONCAT_WS('-', TRIM(LEADING '0' FROM date_format(posted, '%Y')), " +
+		"TRIM(LEADING '0' FROM date_format(posted, '%c')), " +
+		"TRIM(LEADING '0' FROM date_format(posted, '%d')),alias)) AS htmlFileName, " +
+		"views," +
+		"posted " +
+		"FROM tblblogentries " +
+		"ORDER BY posted ASC " +
+		"LIMIT 10";
 
 config.mysql = {
 	host     : 'localhost',
 	user     : 'root',
 	password : '',
-	database : 'blogcfc'
+	database : 'andyjarrett'
 };
 
 module.exports = config;
--- createhtml.js
+++ createhtml.js
@@ -7,46 +7,43 @@ var dateFormat = require('dateformat');
 var config = require('./config');
 
 // MySQL conncetion details
-var connection = mysql.createConnection( config.mysql );
-
-
-
+var connection = mysql.createConnection(config.mysql);
 connection.connect();
-connection.query( config.sql , processData);
+connection.query(config.sql, processData);
 connection.end();
 
 
+
 function processData(err, rows, fields) {
+	var i = 0;
 	if (err) throw err;
 
 	// Loop over the rows
-	for (var i = 0; i < rows.length; i++) {
+	for (i = 0; i < rows.length; i++) {
 
 		var fileName = rows[i].htmlFileName + '.html';
-		var filePath = config.outPath + fileName
+		var filePath = config.outPath + fileName;
 
 		fs.writeFileSync(filePath, createHTMLContent( rows[i],i ));
-
 	}
 }
-
 
 function createHTMLContent( row, pageorder ){
 
 	// Create meta data first, make sure
 	// layout/title/date/pageOrder are at the top
 	var meta = ['---',
-							'layout: blog',
-							'title: "' + escape(row.title) +'"',
-							'date: ' + row.posted,
-							'pageOrder: ' + pageorder,
-							'alias: ' + row.alias,
-							'dateTime: ' + row.posted,
-							'---'];
+				'layout: blog',
+				'title: "' + escape(row.title) +'"',
+				'date: ' + row.posted,
+				'pageOrder: ' + pageorder,
+				'alias: ' + row.alias,
+				'dateTime: ' + row.posted,
+				'---'];
 	// Add the content
-	meta.push( row.fullbody );
+	meta.push(row.fullbody);
 
 	// Output the htmlFileName we are processing
-	console.log(row.htmlFileName)
-	return meta.join('\n')
+	console.log(row.htmlFileName);
+	return meta.join('\n');
 }
